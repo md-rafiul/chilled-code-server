7ea539aef396609b330cebeec1d425661f2651f8
md-rafiul <md.rafiul.pau@gmail.com>
2022-10-26 21:54:42 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = process.env.Port || 5000;
 
 const courseData = require("./resources/resources.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(courseData);
