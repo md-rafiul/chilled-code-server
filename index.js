ce868e42106d25d01934ad3dc0d7ea41a952fd3b
md-rafiul <md.rafiul.pau@gmail.com>
2022-10-26 21:09:09 +0600

    category json data added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.Port || 5000;
 
+const courseData = require("./resources/resources.json");
+
 app.get("/", (req, res) => {
-  res.send("server is running! ");
+  res.send(courseData);
 });
 
 app.listen(port, () => {
