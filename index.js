d5613a0dbcdc8f8d3ecbd34d259462fb7b32d567
md-rafiul <md.rafiul.pau@gmail.com>
2022-10-27 21:18:15 +0600

    server done

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ var cors = require("cors");
 const app = express();
 const port = process.env.Port || 5000;
 
-const courseData = require("./resources/resources.json");
-
+const courseData = require("./resources/courseDetails.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
+  res.send("Assignment 10 server is running!!!");
+});
+
+app.get("/courses", (req, res) => {
   res.send(courseData);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courseData.find((course) => course.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
